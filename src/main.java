f3f6a6533e0d2e91a8948b87672d8b72d974ce37
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-06 13:45:46 -0400

    wrote the rules for pigLatin in a comment

--- src/main.java
+++ src/main.java
@@ -1,6 +1,19 @@
 import javax.swing.JOptionPane;
 public class main {
 	public static void main(String[] args) {
-
+	/**
+	 * CONSTANT RULE
+	 * move the constant to the end of the word, add -ay
+	 * pig-igpay
+	 * lack-acklay
+	 * wack-ackpay
+	 * CONSTANT CLUSTER RULE
+	 * move the cluster of constants to the end, add -ay
+	 * smack-acksmay
+	 * smile-ilesmay
+	 * VOWEL RULE
+	 * just add hay, way, or yay
+	 * https://en.wikipedia.org/wiki/Pig_Latin
+	 */
 	}
 }
