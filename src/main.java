92ea62a1c794d175af51191f2433917a833de5cb
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-06 14:47:47 -0400

    created method FindTheRestOfTheWord()

--- src/main.java
+++ src/main.java
@@ -1,48 +1,41 @@
 import javax.swing.JOptionPane;
+
 public class main {
-	public static String FindConstant(String input)
-	{
+	public static String FindTheRestOfTheWord(String Constants, String input) {
+		return input;
+	}
+	public static String FindConstant(String input) {
 		String OurConstants = "bcdfghjklmnpqrstvwxyz";
 		// this is the first few constants,
-		// like  the P in pig 
+		// like the P in pig
 		// or the SM in smile
 		String firstConstants = "";
 		int count = 0;
 		boolean FoundVowel = false;
-		while(FoundVowel == false && count < input.length())
-		{
+		while (FoundVowel == false && count < input.length()) {
 			int result = OurConstants.indexOf(input.charAt(count));
-			if(result > -1)
-			{
+			if (result > -1) {
 				firstConstants += input.charAt(count);
-			}
-			else
-			{
+			} else {
 				FoundVowel = true;
 			}
 			count++;
 		}
 		return firstConstants;
 	}
+
 	public static void main(String[] args) {
-	/**
-	 * CONSTANT RULE
-	 * move the constant to the end of the word, add -ay
-	 * pig-igpay
-	 * lack-acklay
-	 * wack-ackpay
-	 * CONSTANT CLUSTER RULE
-	 * move the cluster of constants to the end, add -ay
-	 * smack-acksmay
-	 * smile-ilesmay
-	 * VOWEL RULE
-	 * just add hay, way, or yay
-	 * https://en.wikipedia.org/wiki/Pig_Latin
-	 */
+		/**
+		 * CONSTANT RULE move the constant to the end of the word, add -ay
+		 * Example: pig-igpay, lack-acklay, wack-ackpay 
+		 * CONSTANT CLUSTER RULE move the cluster of constants
+		 * to the end, add -ay smack-acksmay smile-ilesmay VOWEL RULE just add hay, way,
+		 * or yay https://en.wikipedia.org/wiki/Pig_Latin
+		 */
 		String input = JOptionPane.showInputDialog(null, "What message would you like converted to pig latin?");
-		String result = FindConstant(input);
-		System.out.print(result);
+		String OurConstants = FindConstant(input); //Stores the first 1-2 constants
+		String TheRest = FindTheRestOfTheWord(OurConstants, input);
 		JOptionPane.showMessageDialog(null, input);
-		
+
 	}
 }
